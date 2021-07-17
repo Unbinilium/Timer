e29f465d40eee33a1fd408329e45be23d644fe0b
Unbinilium <unbinilium@outlook.com>
2021-07-17 22:08:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# Timer
+
+A simple thread-safe timer util library written in stdandared C++ 20, for measuring and recording the time spent performing different functions.
+
+```cpp
+int main() {
+    ubn::timer t("Main therad");
+    std::jthread([&] {
+        doSomeThing(1s);
+        t.setTag("Sub thread", "Thread 1");
+    });
+    std::jthread([&] {
+        doSomeThing(0.5s);
+        t.setTag("Sub thread", "Thread 2");
+    });
+    doSomeThing(0.2s);
+    t.setTag("Thread 1", "Thread 2");
+    t.printAllInfoHistory();
+}
+```
+
+To use timer util library with your project, please clone this repository as git submodule and add these to your `CMakeLists.txt`:
+
+```cmake
+add_subdirectory(<time source dir>)
+target_link_libraries(<your project name> timer)
+```
+
+## Class
+
+Defined in header `timer.hpp`, namespace `ubn`:
+
+```cpp
+template <
+	typename T = std::chrono::high_resolution_clock,
+	typename P = std::chrono::milliseconds
+> class timer;
+```
+
+## Member functions
+
+- Constructor
+
+If init a timer with a specified `_self_tag_name`, it will print all its info history with this tag name after the timer was destructed. The default info history records limit is set to `5` by `_info_history_size`, it should always large than `0`.
+
+ ```cpp
+explicit timer(
+    const std::string&& _self_tag_name = "",
+    const std::size_t _info_history_size = 5
+)
+
+explicit timer(
+    const std::map<std::string, std::chrono::time_point<T>>&& _time_point_map,
+    const std::string&& _self_tag_name,
+    const std::size_t _info_history_size
+)
+ ```
+
+- Operator
+
+Update duration(s) by substract another timer.
+
+```cpp
+void operator-(timer& _timer)
+```
+
+- General
+
+| Function name                            | Design purpose                                               |
+| :--------------------------------------- | :----------------------------------------------------------- |
+| `auto setTag(const char* ...)`           | If the tag(s) was not set, then set new tag(s) and initialize duration, else update the tag(s) time and update duration, returns time point |
+| `auto getTimePoint(const char*)`         | Get time point by tag name                                   |
+| `auto getAllTimePoint()`                 | Get all time point(s), returns time point map                |
+| `bool eraseTag(const char* ...)`         | Erase tag(s) by tag name(s), returns `false` if some tag names not found, otherwise is `true` |
+| `auto getDuration(const char*)`          | Get duration by tag name                                     |
+| `auto getAllDuration()`                  | Get all duration(s), returns duration map                    |
+| `auto getInfo(const char*)`              | Get tag info by tag name, returns an unordered map that includes the last updated info |
+| `void printInfo(const char* ...)`        | Print last updated info by tag name(s)                       |
+| `auto getInfoHistory(const char*)`       | Get tag info history by tag name, returns a deque that includes all the info history of this tag |
+| `void printInfoHistory(const char* ...)` | Print tag info history recored(s) by tag name(s), the default size of info history records is `5` |
+| `void printAllInfoHistory()`             | Print all info history record(s) for all tag(s)              |
+| `bool clearInfoHistory(const char* ...)` | Clear tag(s) info history record(s) by tag name(s), returns `false` if some tag names not found, otherwise is `true` |
+| `void clear()`                           | Clear time point map, duration map and info history map      |
+
+## License
+
+MIT License Copyright (c) 2021 Unbinilium.
---
+++ include/timer.hpp
@@ -0,0 +1,306 @@
+#pragma once
+
+#include <atomic>
+#include <chrono>
+#include <cstddef>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <utility>
+
+namespace ubn {
+   namespace timer_concept {
+        template <typename T>
+        concept const_char_pointer = std::is_nothrow_convertible_v<T, const char*>;
+   }
+
+    template <typename T = std::chrono::high_resolution_clock, typename P = std::chrono::milliseconds>
+    class timer {
+    public:
+        explicit timer(
+            const std::string&& _self_tag_name = "",
+            const std::size_t _info_history_size = 5
+        ) : m_self_tag_name(_self_tag_name), m_info_history_size(_info_history_size) {
+            setTag(m_self_tag_name.c_str());
+        }
+
+        explicit timer(
+            const std::map<std::string, std::chrono::time_point<T>>&& _time_point_map,
+            const std::string&& _self_tag_name,
+            const std::size_t _info_history_size
+        ) : m_time_point_map(_time_point_map), m_self_tag_name(_self_tag_name), m_info_history_size(_info_history_size) {}
+
+        ~timer() {
+            if (!m_self_tag_name.empty()) {
+                setTag(m_self_tag_name.c_str());
+                printInfoHistory(m_self_tag_name.c_str());
+            }
+        }
+
+        inline void operator-(timer& _timer) {
+            const ticket_guard tg(this);
+            for (const auto& [key, _] : m_time_point_map) {
+                const auto duration {
+                    std::chrono::duration_cast<P>(m_time_point_map[key] - _timer.getTimePoint(key.c_str()))
+                };
+                updateInfoHistory(key.c_str(), std::move(duration));
+            }
+        }
+
+        inline auto setTag(const char* _tag_name) noexcept {
+            const auto time_point { T::now() };
+            const ticket_guard tg(this);
+            if (m_time_point_map.contains(_tag_name)) {
+                const auto duration {
+                    std::chrono::duration_cast<P>(time_point - m_time_point_map[_tag_name])
+                };
+                m_duration_map.insert_or_assign(_tag_name, duration);
+                m_time_point_map[_tag_name] = time_point;
+                updateInfoHistory(_tag_name, std::move(duration));
+            } else {
+                m_time_point_map.emplace(_tag_name, time_point);
+                initInfoHistory(_tag_name, time_point);
+            }
+            return time_point;
+        }
+
+        template <timer_concept::const_char_pointer Arg, timer_concept::const_char_pointer... Args>
+        inline auto setTag(const Arg& _arg, const Args&... _args) {
+            setTag(_arg);
+            return setTag(std::forward<const Args>(_args)...);
+        }
+
+        inline auto getTimePoint(const char* _tag_name) {
+            const ticket_guard tg(this);
+            return m_time_point_map.contains(_tag_name)
+                ? m_time_point_map[_tag_name]
+                : T::now();
+        }
+
+        inline auto getAllTimePoint() {
+            const ticket_guard tg(this);
+            return m_time_point_map;
+        }
+
+        inline bool eraseTag(const char* _tag_name) {
+            const ticket_guard tg(this);
+            return eraseTimePoint(_tag_name) &&
+                eraseDuration(_tag_name) &&
+                eraseInfoHistory(_tag_name);
+        }
+
+        template <timer_concept::const_char_pointer Arg, timer_concept::const_char_pointer... Args>
+        inline bool eraseTag(const Arg& _arg, const Args&... _args) {
+            return eraseTag(_arg) & eraseTag(std::forward<const Args>(_args)...);
+        }
+
+        inline auto getDuration(const char* _tag_name) {
+            const ticket_guard tg(this);
+            return m_duration_map.contains(_tag_name)
+                ? m_duration_map[_tag_name]
+                : P();
+        }
+
+        inline auto getAllDuration() {
+            const ticket_guard tg(this);
+            return m_duration_map;
+        }
+
+        inline auto getInfo(const char* _tag_name) {
+            const ticket_guard tg(this);
+            return m_info_history_map.contains(_tag_name)
+                ? m_info_history_map[_tag_name].back()
+                : std::unordered_map<std::string, double>();
+        }
+
+        inline void printInfo(const char* _tag_name) {
+            const ticket_guard tg(this);
+            if (m_info_history_map.contains(_tag_name)) {
+                printInfo(_tag_name, m_info_history_map[_tag_name].back());
+            }
+        }
+
+        template <timer_concept::const_char_pointer Arg, timer_concept::const_char_pointer... Args>
+        inline void printInfo(const Arg& _arg, const Args&... _args) {
+            printInfo(_arg);
+            printInfo(std::forward<const Args>(_args)...);
+        }
+
+        inline void printAllInfo() {
+            const ticket_guard tg(this);
+            printAllInfo(m_duration_map);
+        }
+
+        inline auto getInfoHistory(const char* _tag_name) {
+            const ticket_guard tg(this);
+            return m_info_history_map.contains(_tag_name)
+                ? m_info_history_map[_tag_name]
+                : std::deque<std::unordered_map<std::string, double>>();
+        }
+
+        inline void printInfoHistory(const char* _tag_name) {
+            const ticket_guard tg(this);
+            if (m_info_history_map.contains(_tag_name)) {
+                for (auto& info_history : m_info_history_map[_tag_name]) {
+                    printInfo(_tag_name, info_history);
+                }
+            }
+        }
+
+        template <timer_concept::const_char_pointer Arg, timer_concept::const_char_pointer... Args>
+        inline void printInfoHistory(const Arg& _arg, const Args&... _args) {
+            printInfoHistory(_arg);
+            printInfoHistory(std::forward<const Args>(_args)...);
+        }
+
+        inline void printAllInfoHistory() {
+            const ticket_guard tg(this);
+            printAllInfoHistory(m_duration_map);
+        }
+
+        inline bool clearInfoHistory(const char* _tag_name) {
+            const ticket_guard tg(this);
+            const auto status { eraseInfoHistory(_tag_name) };
+            if (status) { initInfoHistory(_tag_name); }
+            return std::move(status);
+        }
+
+        template <timer_concept::const_char_pointer Arg, timer_concept::const_char_pointer... Args>
+        inline bool clearInfoHistory(const Arg& _arg, const Args&... _args) {
+            return clearInfoHistory(_arg) & clearInfoHistory(std::forward<const Args>(_args)...);
+        }
+
+        inline void clear() {
+            const ticket_guard tg(this);
+            m_time_point_map.clear();
+            m_duration_map.clear();
+            m_info_history_map.clear();
+        }
+
+    protected:
+        inline void initInfoHistory(const char* _tag_name, const std::chrono::time_point<T>& _time_point) noexcept {
+            std::unordered_map<std::string, double> info;
+            info.emplace("time_point_at", _time_point.time_since_epoch().count());
+            for (const auto& key : { "id", "cur_duration", "min_duration", "max_duration", "avg_duration", "frequency" }) {
+                info.emplace(key, 0.);
+            }
+            std::deque<std::unordered_map<std::string, double>> info_history { info };
+            m_info_history_map.emplace(_tag_name, std::move(info_history));
+        }
+
+        inline void updateInfoHistory(const char* _tag_name, const P& _duration) noexcept {
+            const auto duration_count { _duration.count() };
+            auto info { m_info_history_map[_tag_name].back() };
+            if (static_cast<std::size_t>(info["id"]) == 0) {
+                for (const auto& key : { "min_duration", "max_duration", "avg_duration" }) {
+                    info[key] = duration_count;
+                }
+            } else {
+                if (info["min_duration"] > duration_count) {
+                    info["min_duration"] = duration_count;
+                }
+                if (info["max_duration"] < duration_count) {
+                    info["max_duration"] = duration_count;
+                }
+                info["avg_duration"] = (info["avg_duration"] + duration_count) / 2.;
+            }
+            info["id"] += 1.;
+            info["time_point_at"] = m_time_point_map[_tag_name].time_since_epoch().count();
+            info["cur_duration"] = duration_count;
+            info["frequency"] = 1. / std::chrono::duration<double, std::ratio<1>>(_duration).count();
+            while (m_info_history_map[_tag_name].size() >= m_info_history_size) {
+                m_info_history_map[_tag_name].pop_front();
+            }
+            m_info_history_map[_tag_name].push_back(std::move(info));
+        }
+
+        inline void printInfo(const char* _tag_name, std::unordered_map<std::string, double>& _info_history) {
+            std::cout << "[timer] Info '"
+                << _tag_name << "' -> "
+                << static_cast<std::size_t>(_info_history["id"]) << " set at: "
+                << static_cast<std::size_t>(_info_history["time_point_at"]) << " duration (cur/min/max/avg): "
+                << _info_history["cur_duration"] << "/"
+                << _info_history["min_duration"] << "/"
+                << _info_history["max_duration"] << "/"
+                << _info_history["avg_duration"] << ", frequency: "
+                << _info_history["frequency"] << std::endl;
+        }
+
+        inline void printAllInfo(const std::map<std::string, P>& _duration_map) {
+            for (const auto& [key, _] : _duration_map) {
+                if (m_info_history_map.contains(key)) {
+                    printInfo(key.c_str(), m_info_history_map[key].back());
+                }
+            }
+        }
+
+        inline void printAllInfoHistory(const std::map<std::string, P>& _duration_map) {
+            for (const auto& [key, _] : _duration_map) {
+                for (auto& info_history : m_info_history_map[key]) {
+                    printInfo(key.c_str(), info_history);
+                }
+            }
+        }
+
+        inline bool eraseTimePoint(const char* _tag_name) {
+            if (m_time_point_map.contains(_tag_name)) {
+                m_time_point_map.erase(_tag_name);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        inline bool eraseDuration(const char* _tag_name) {
+            if (m_duration_map.contains(_tag_name)) {
+                m_duration_map.erase(_tag_name);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        inline bool eraseInfoHistory(const char* _tag_name) {
+            if (m_info_history_map.contains(_tag_name)) {
+                m_info_history_map.erase(_tag_name);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    private:
+        inline void lock() noexcept {
+            const auto ticket { m_ticket_in.fetch_add(1, std::memory_order::acquire) };
+            while (true) {
+                const auto now { m_ticket_out.load(std::memory_order::acquire) };
+                if (now == ticket) { return; }
+                m_ticket_out.wait(now, std::memory_order::relaxed);
+            }
+        }
+
+        inline void unlock() noexcept {
+            m_ticket_out.fetch_add(1, std::memory_order::release);
+            m_ticket_out.notify_all();
+        }
+
+        struct ticket_guard {
+            inline ticket_guard(timer* const _timer) noexcept : m_timer(_timer) { m_timer->lock(); }
+            inline ~ticket_guard() noexcept { m_timer->unlock(); }
+            timer* const m_timer { nullptr };
+        };
+
+        std::string const m_self_tag_name;
+        std::size_t m_info_history_size;
+
+        std::map<std::string, std::chrono::time_point<T>> m_time_point_map;
+        std::map<std::string, P> m_duration_map;
+        std::map<std::string, std::deque<std::unordered_map<std::string, double>>> m_info_history_map;
+
+        alignas(2 * sizeof(std::max_align_t)) mutable std::atomic<std::size_t> m_ticket_in;
+        alignas(2 * sizeof(std::max_align_t)) mutable std::atomic<std::size_t> m_ticket_out;
+    };
+}
